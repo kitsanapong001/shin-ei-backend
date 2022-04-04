cc43aae06ee9e91335847bb78f84ca4d98d323b9
kitsanapong001 <kitsanapong001@gmail.com>
2022-04-04 16:25:34 +0700

    update jwt

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -91,7 +91,7 @@ exports.signin = (req, res) => {
 
       var token = jwt.sign({ id: user.id }, config.secret, {
         // expiresIn: 86400, // 24 hours
-        expiresIn: 10, // 1 hours
+        expiresIn: "1h", // 1 hours
       });
 
       var authorities = [];
